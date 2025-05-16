b320267fe4d48e7b7cb492fa66eba0cd61e3e3eb
SasaJovanovicEL <sasa.jovanovic@eaglelab.com>
2025-05-16 15:53:17 +0200

    Initial version

---
+++ ScratchOff.xcframework/ios-arm64/ScratchOff.framework/Headers/ScratchOff.h
@@ -0,0 +1,18 @@
+//
+//  ScratchOff.h
+//  ScratchOff
+//
+//  Created by Nebojsa Nikolic on 11.3.25..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScratchOff.
+FOUNDATION_EXPORT double ScratchOffVersionNumber;
+
+//! Project version string for ScratchOff.
+FOUNDATION_EXPORT const unsigned char ScratchOffVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScratchOff/PublicHeader.h>
+
+
